9537047fe570a7ead2c675ec1b372a742f2b7ef2
Deepak SHAW <deepak.shaw@gmail.com>
2019-11-18 12:34:32 +1100

    BLOB2BLOB working application

---
+++ Blob2Blob/Blob2Blob.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Blob2Blob
+{
+    public static class Blob2Blob
+    {
+        [FunctionName("BlobToBlob")]
+        [return: Blob("%Blob2Blob_Out%/archive-{rand-guid}-{name}")]
+        public static string Run(
+            [BlobTrigger("%Blob2Blob_In%/DK-{name}", Connection = "")]Stream inboundBlob,
+            [Blob("%Blob2Blob_In%/RS-{name}")] string readSecondBlob,
+            [Blob("%Blob2Blob_Out%/Jag-{rand-guid}.txt")] out string returnJag,
+            [Blob("%Blob2Blob_Out%/Div-{rand-guid}.txt")] out string returnDiv,
+            string name, ILogger log)
+        {
+            string vReturn = string.Empty;
+            returnDiv = "";
+            returnJag = "";
+            log.LogInformation($"File recived in Inbound Blob\n Name:{name} \n Size: {inboundBlob.Length} Bytes");
+            try
+            {
+                StreamReader reader = new StreamReader(inboundBlob);
+                string strInboundBlob = reader.ReadToEnd();
+                vReturn = strInboundBlob;
+
+                if (readSecondBlob.Contains("DIV", comparisonType: StringComparison.OrdinalIgnoreCase))
+                {
+                    returnDiv = strInboundBlob + Environment.NewLine + readSecondBlob;
+                }
+                else if (readSecondBlob.Contains("JAG", comparisonType: StringComparison.OrdinalIgnoreCase))
+                {
+                    returnJag = "Jags:" +Environment.OSVersion +
+                                Environment.NewLine + readSecondBlob;
+                }
+                else
+                {
+                    returnDiv = $"Machine: {Environment.MachineName} deosn't have value for Div";  
+                    returnJag = $"Current Directory: {Environment.CurrentDirectory} deosn't have value for Jag";
+                }
+            }
+            catch (Exception ex)
+            {
+                vReturn = $"Process has error [{ex.Message}].{Environment.NewLine}Completed unsuccessfully..";
+                log.LogError($"Exception:{ex.Message}");
+            }
+            return vReturn;
+
+        }
+    }
+}
